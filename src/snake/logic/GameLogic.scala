ded0031a7e87397996a5da341b745c0d7aa95126
Aaron-Serpilin <serpilin.aaron@gmail.com>
2023-09-12 16:49:28 +0200

    Snake Body increases after an apple has been eaten

--- src/snake/logic/GameLogic.scala
+++ src/snake/logic/GameLogic.scala
@@ -5,56 +5,79 @@ import snake.game.SnakeGame
 import snake.logic.GameLogic._
 
 class GameLogic(val random: RandomGenerator,
-                val gridDims : Dimensions) {
-  private var currentDirection : Direction = East() //Set starting position to East/Right
+                val gridDims: Dimensions) {
+  private var currentDirection: Direction = East()
   private var xHeadPosition = 5
   private var yHeadPosition = 3
   private var headPosition = Point(xHeadPosition, yHeadPosition)
-  private var applePosition = Point(random.randomInt(gridDims.width - 1), random.randomInt(gridDims.height -1))
+  private var applePosition = Point(random.randomInt(gridDims.width - 1), random.randomInt(gridDims.height - 1))
   private var appleCount = 0
+  private var snakeBody: List[Point] = List( //Initially define the snake to have a head and two body parts
+    headPosition,
+    Point(xHeadPosition - 1, yHeadPosition),
+    Point(xHeadPosition - 2, yHeadPosition)
+  )
 
   def gameOver: Boolean = false
 
-  // TODO implement me
   def step(): Unit = {
     headPosition = snakeMovement(headPosition, currentDirection)
     xHeadPosition = headPosition.x
     yHeadPosition = headPosition.y
+
+    if (hasAppleBeenEaten()) {
+      appleCount += 1
+      applePosition = Point(random.randomInt(gridDims.width - 1), random.randomInt(gridDims.height - 1))
+      snakeBodyExtender()
+    } else {
+      snakeBody = headPosition :: snakeBody.dropRight(1) //Adds all the elements to the body except the tail to make space for the head
+    }
   }
 
-  // TODO implement me
+
+  // Implement logic to set reverse direction
   def setReverse(r: Boolean): Unit = ()
 
-  // TODO implement me
   def changeDir(d: Direction): Unit = {
     currentDirection = d
   }
 
-  // TODO implement me
   def getCellType(p: Point): CellType = {
     if (headPosition == p) {
       SnakeHead(currentDirection)
     } else if (applePosition == p) {
       Apple()
-    } else if (applePosition == headPosition) {
-      appleCount += 1
-      applePosition = Point(random.randomInt(gridDims.width - 1), random.randomInt(gridDims.height - 1))
+    } else if (hasAppleBeenEaten()) {
       SnakeHead(currentDirection)
+    } else if (snakeBody.contains(p)) {
+      SnakeBody(0) //Define color of snakeBody
     } else {
       Empty()
     }
   }
 
-  def snakeMovement(position: Point, direction: Direction): Point = {
-    direction match { //The % ensures wraparound
-      case East() => Point((position.x + 1) % gridDims.width, position.y)
-      case West() => Point((position.x - 1 + gridDims.width) % gridDims.width, position.y)
+  private def snakeMovement(position: Point, direction: Direction): Point = {
+    direction match { //The modulo allows for the wraparound since if it surpasses the gridDims, it redefines based on the remainder
+      case East()  => Point((position.x + 1) % gridDims.width, position.y)
+      case West()  => Point((position.x - 1 + gridDims.width) % gridDims.width, position.y)
       case North() => Point(position.x, (position.y - 1 + gridDims.height) % gridDims.height)
       case South() => Point(position.x, (position.y + 1) % gridDims.height)
     }
   }
 
+  private def hasAppleBeenEaten (): Boolean = {
+    if (headPosition == applePosition) {
+      return true
+    }
+    false
+  }
+
+  private def snakeBodyExtender(): Unit = {
+    val newBlocks = List.fill(3)(headPosition)
+    snakeBody = newBlocks ::: snakeBody //We don't add but make a new list with the new blocks since lists are immutable in Scala
+  }
 }
+
 
 /** GameLogic companion object */
 object GameLogic {
@@ -75,10 +98,8 @@ object GameLogic {
   // In your code only use gridDims.width and gridDims.height
   // do NOT use DefaultGridDims.width and DefaultGridDims.height
   val DefaultGridDims
-    : Dimensions =
-    Dimensions(width = 25, height = 25)  // you can adjust these values to play on a different sized board
-
-
+  : Dimensions =
+  Dimensions(width = 25, height = 25)  // you can adjust these values to play on a different sized board
 
 }
 
