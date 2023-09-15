df9cbf47a5856e23d2765a4038e64d59df140f34
Aaron-Serpilin <serpilin.aaron@gmail.com>
2023-09-15 13:33:21 +0200

    New Snake Body approach using its length. Passing 16 tests

--- src/snake/logic/GameLogic.scala
+++ src/snake/logic/GameLogic.scala
@@ -10,50 +10,60 @@ class GameLogic(val random: RandomGenerator,
   private var xHeadPosition = 2
   private var yHeadPosition = 0
   private var headPosition = Point(xHeadPosition, yHeadPosition)
-  private var snakeBody: List[Point] = List( //Initially define the snake to have a head and two body parts
+  private var snakeBody: List[Point] = List (//Initially define the snake to have a head and two body parts
     headPosition,
     Point(xHeadPosition - 1, yHeadPosition),
     Point(xHeadPosition - 2, yHeadPosition)
   )
-  private var applePosition = appleGenerator() //Initialize it to something
+  var snakeLength = snakeBody.length
+  var entireGrid: Seq[Point] = Seq()
+  var emptyCells: List[Point] = List()
+  private var applePosition = appleGenerator()
   private var gameConcluded = false
 
 
-  //apple is random now
   def gameOver: Boolean = gameConcluded
 
   def step(): Unit = {
-    println(s"The apple location is ${applePosition}")
+    //println(s"The apple location is ${applePosition}")
     if (collisionDetector()) {gameConcluded = true}
     headPosition = snakeMovement(headPosition, currentDirection)
     xHeadPosition = headPosition.x
     yHeadPosition = headPosition.y
 
-    if (hasAppleBeenEaten()) {
-      applePosition = Point(random.randomInt(gridDims.width - 1), random.randomInt(gridDims.height - 1))
-      snakeBodyExtender()
-    }
+    snakeBody = headPosition :: snakeBody.take(snakeLength - 1)
 
-    snakeBody = headPosition :: snakeBody.dropRight(1) //Adds all the elements to the body except the tail to make space for the head
+    if (hasAppleBeenEaten()) {
+      applePosition = appleGenerator()
+      snakeLength += 3 //We increase the snake length so that every step a new element is added to the body. This way the snake increases a block during each step and not all at once
+    }
 
   }
 
-
-  // Implement logic to set reverse direction
   def setReverse(r: Boolean): Unit = ()
 
   def changeDir(d: Direction): Unit = {
-    currentDirection = d
-    //Check for opposites
+    if (!oppositeDirectionDetector(d, currentDirection)) {
+      currentDirection = d
+    }
   }
+
+  private def oppositeDirectionDetector(previousDirection: Direction, currentDirection: Direction): Boolean = {
+    (previousDirection, currentDirection) match {
+      case (East(), West()) => true
+      case (West(), East()) => true
+      case (North(), South()) => true
+      case (South(), North()) => true
+      case _ => false
+    }
+  }
+
 
   def getCellType(p: Point): CellType = {
     if (headPosition == p) {
       SnakeHead(currentDirection)
-    } else if (applePosition == p) {
+    } else if (applePosition == p && emptyCells.nonEmpty) {
       Apple()
-    } else if (hasAppleBeenEaten()) {
-      SnakeHead(currentDirection)
     } else if (snakeBody.contains(p)) {
       val snakeBodyIndex = snakeBody.indexOf(p)
       val colorIndex = snakeBodyIndex.toFloat / (snakeBody.length - 1)
@@ -61,6 +71,10 @@ class GameLogic(val random: RandomGenerator,
     } else {
       Empty()
     }
+
+//    else if (hasAppleBeenEaten()) {
+//      SnakeHead(currentDirection)
+//    }
   }
 
   private def snakeMovement(position: Point, direction: Direction): Point = {
@@ -79,57 +93,33 @@ class GameLogic(val random: RandomGenerator,
     false
   }
 
-  private def snakeBodyExtender(): Unit = {
-    for (x <- 0 until 3) {
-      snakeBody = snakeBody :+ snakeBody.last
-    }
-  }
-
-
   private def appleGenerator(): Point = {
-    val entireGrid = gridDims.allPointsInside
-    val emptyCells = entireGrid.filterNot(p => snakeBody.contains(p)).toList
-
-    if (emptyCells.nonEmpty) {
+    entireGrid = gridDims.allPointsInside
+    emptyCells = entireGrid.filterNot(p => snakeBody.contains(p)).toList
+    //println(s"The number of empty cells is ${emptyCells.length}")
+    if (emptyCells.nonEmpty) { //Applies when there are available free cells
       val randomIndex = random.randomInt(emptyCells.length)
+      //println(s"The index is ${randomIndex}")
       emptyCells(randomIndex)
-    } else {
-      Point(random.randomInt(gridDims.width), random.randomInt(gridDims.height))
+    } else { //Applies when there is no place for the apple to spawn
+      //println("-1 invoked")
+      Point(-1, -1)
     }
   }
 
 
   private def collisionDetector(): Boolean = {
-
-    if (snakeBody.tail.contains(headPosition)) {
-      return true
-    }
+    if (snakeBody.tail.contains(headPosition)) {return true}
     false
   }
-
 }
 
 
 /** GameLogic companion object */
 object GameLogic {
-
-  val FramesPerSecond: Int = 5 // change this to increase/decrease speed of game
-
-  val DrawSizeFactor = 1.0 // increase this to make the game bigger (for high-res screens)
-  // or decrease to make game smaller
-
-  // These are the dimensions used when playing the game.
-  // When testing the game, other dimensions are passed to
-  // the constructor of GameLogic.
-  //
-  // DO NOT USE the variable DefaultGridDims in your code!
-  //
-  // Doing so will cause tests which have different dimensions to FAIL!
-  //
-  // In your code only use gridDims.width and gridDims.height
-  // do NOT use DefaultGridDims.width and DefaultGridDims.height
+  val FramesPerSecond: Int = 1
+  val DrawSizeFactor = 1.0
   val DefaultGridDims
   : Dimensions =
-  Dimensions(width = 25, height = 25)  // you can adjust these values to play on a different sized board
-
+  Dimensions(width = 6, height = 1)
 }
